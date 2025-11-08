311706ee95a4759899cfb582747fb051abc79058
arthsonani <arthsonani@gmail.com>
2025-11-08 21:53:25 +0530

    app design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Whiteboard from "./components/Whiteboard";
+import MainLayout from "./layouts/MainLayout";
+import AuthLayout from "./layouts/AuthLayout";
+import ViewWhiteboard from './components/ViewWhiteboard';
 
-function App() {
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/board/:boardId" element={<Whiteboard />} />
+                    <Route path="/viewBoard/:boardId" element={<ViewWhiteboard />} />
+                </Route>
 
-  return (
-    <>
-     App
-    </>
-  )
-}
+                {/* Routes without Navbar & Footer */}
+                <Route element={<AuthLayout />}>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
+};
 
-export default App
+export default App;
