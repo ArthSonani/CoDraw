ab0f7fa22dd4b907f959b4a3685959515c731526
arthsonani <arthsonani@gmail.com>
2025-11-08 21:57:12 +0530

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Footer(){ return (
+    <footer className="bg-black text-white font-mono p-4 text-center flex items-center justify-between">
+      <div className="flex items-center gap-3">
+        <div className="p-2 bg-gray-200"><img src="/logo.png" alt="" width={100}/></div>
+        <p>&copy; CoDraw {new Date().getFullYear()} | All Rights Reserved.</p>
+      </div>
+      <p>Designed and Developed by <a href="https://github.com" target="_blank"><span className="underline">Arth Dholakia</span></a></p>
+    </footer>
+  );}
